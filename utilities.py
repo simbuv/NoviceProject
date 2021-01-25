2f1c8f8a99221ea683cc23b3bcf455c39b2f2827
Silambarasan V <simbu.v@gmail.com>
2021-01-25 00:25:21 -0500

    second edit utilities.py

--- utilities.py
+++ utilities.py
@@ -12,3 +12,22 @@ p1 = listoperations("Simbu",36)
 print(p1.name)
 print(p1.age)
 print(p1.myName())
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    
+    def printname(self):
+        print(self.firstname,self.firstname)
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname,lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("welcome",self.firstname,self.lastname, "to the class of",self.graduationyear)
+
+x = Student("Simbu", "San", 2019)
+x.welcome()
